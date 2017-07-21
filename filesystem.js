5fe2b92fa78bc99481d19ef46bb3a037f9c8c0e0
zvakanaka <quintonish@gmail.com>
2017-07-21 11:29:30 -0500

    setup for demo

--- filesystem.js
+++ filesystem.js
@@ -1,32 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const DEMO = require('./demo.json');
+
 function getFilesInDirectory(base='./', album='', fileExtensions=['jpg']) {
-  const directory = `${base}/${album}`;
-  if (!fs.existsSync(directory)) return { err: `directory "${directory}" does not exist` };
+  const directory = `${album}`;
+  if (typeof DEMO[directory] == 'undefined') return { err: `directory "${directory}" does not exist` };
   const extensions = fileExtensions.map(ext => `.${ext.toLowerCase()}`);
-  let files = fs.readdirSync(directory)
-    .filter(function(file) {
-      if (extensions.indexOf(path.extname(file).toLowerCase())>-1) return true;
-    });
-  return files;
+  return files = DEMO[directory]
+    .filter(file => extensions.indexOf(path.extname(file).toLowerCase())>-1);
 }
 
 function getDirectoriesInDirectory(base='./', ignore=[]) {
-  if (!fs.existsSync(base)) return { err: `directory "${base}" does not exist`};
-  let directories = fs.readdirSync(base)
-    .filter(file => {
-      if (fs.statSync(`${base}/${file}`).isDirectory()
-        && (ignore.indexOf(file)===-1)) return true;
-    });
-  return directories;
+  return Object.keys(DEMO);
 }
 
 module.exports = {
   getFilesInDirectory: getFilesInDirectory,
   getDirectoriesInDirectory: getDirectoriesInDirectory
 };
-// let images = getFilesInDirectory('/Users/qui10001/Desktop', '', ['png']);
-// console.log('IMAGES', images);
-// let dirs = getDirectoriesInDirectory('/Users/qui10001/Desktop', ['hide_me']);
-// console.log('DIRS', dirs);
